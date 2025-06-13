e2a4c51b1f58fad23c7eedace9bc7e1f6b625f47
Milleys <johnwilsonlorin1@gmail.com>
2025-06-13 18:54:02 +0800

    new commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+//named routes
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name("dashboard-page");
+
+
+//grouped routes
+
